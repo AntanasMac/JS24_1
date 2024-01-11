bbf405c93cb81e31bceb8a88e78ade2a4c2e1032
AntanasMac <antanas.maciulskis@gmail.com>
2024-01-11 22:53:16 +0200

    forgot to add string_methods and string_slicing

---
+++ WEBSITE/string_methods.js
@@ -0,0 +1,14 @@
+let userName = "Makaka";
+
+/* variable_with_string.charAt(0); //parodo raide toje vietoje kur 0
+tai reiskia parodys pirma raide.
+
+indexOf("k")
+
+yra ir daugiau visokiu, bet nepasirode itin aktualus siam momentui,
+manau svarbu zinoti, kad kai sukuri variable ir jam priskiri string, tada
+gali naudoti string metodus po tasko. variableName.charAt(4); ir 
+daugeli kitu.
+
+*/
+
---
+++ WEBSITE/string_slicing.js
@@ -0,0 +1,16 @@
+let stringVariable = "vabalas";
+
+console.log(stringVariable.slice(0,3));
+    // result is "vab"
+
+
+
+    // ***** example 2 - extracing from-to
+const email = "auksinisKardas@gmail.com";
+let username = email.slice(0, email.indexOf("@"));
+
+console.log (username);
+    // output is auksinisKardas
+
+
+    
